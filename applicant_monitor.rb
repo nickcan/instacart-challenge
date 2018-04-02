36dd2e6f422aee5365e74081383bba13435903a3
Nicholas Cantelmi <nickcantelmi@gmail.com>
2018-04-01 17:21:12 -0700

    Initial Commit for creating a CSV script

---
+++ applicant_monitor.rb
@@ -0,0 +1,83 @@
+require "active_record"
+require "csv"
+require "sqlite3"
+
+File.delete("monitor.csv")
+
+START_DATE_INPUT = ARGV.first
+END_DATE_INPUT = ARGV.last
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database  => "./applicants.sqlite3"
+)
+
+class Applicant < ActiveRecord::Base
+  POSSIBLE_WORKFLOW_STATES = [
+    :applied,
+    :quiz_started,
+    :quiz_completed,
+    :onboarding_requested,
+    :onboarding_completed,
+    :hired
+  ].freeze
+end
+
+class ApplicantMonitor
+  attr_reader :start_date, :end_date
+
+  DATE_FORMAT = "%Y-%m-%d"
+
+  def initialize(start_range = "", end_range = "")
+    @start_date = start_range.to_date.beginning_of_day.end_of_week(:tuesday)
+    @end_date = end_range.to_date.end_of_day
+  end
+
+  def write_csv
+    CSV.open("monitor.csv", "a+") do |csv|
+      csv << ["count", "week", "workflow_state"]
+
+      num_of_weeks.times do |week_int|
+        interval = one_week_interval(week_int)
+        workflow_state_counts = default_aggregated_counts_by_workflow_state
+
+        applicants_of_specific_interval(interval).each do |applicant|
+          workflow_state_counts[applicant.workflow_state] += 1
+        end
+
+        workflow_state_counts.each do |key, val|
+          csv << [val, interval.first, key]
+        end
+      end
+    end
+  end
+
+  private
+
+  def default_aggregated_counts_by_workflow_state
+    Applicant::POSSIBLE_WORKFLOW_STATES.reduce({}) do |map, state|
+      map[state.to_s] = 0
+      map
+    end
+  end
+
+  def one_week_interval(week_int)
+    beginning_of_interval = start_date + week_int.week
+
+    beginning_of_interval.strftime(DATE_FORMAT)..
+    (beginning_of_interval + 1.week).strftime(DATE_FORMAT)
+  end
+
+  def num_of_weeks
+    float_weeks = (end_date - start_date)/1.week
+    float_weeks.floor
+  end
+
+  def applicants_of_specific_interval(interval)
+    Applicant.where(created_at: interval)
+  end
+end
+
+monitor = ApplicantMonitor.new(START_DATE_INPUT, END_DATE_INPUT)
+
+monitor.write_csv
