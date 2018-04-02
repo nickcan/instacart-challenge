976ecc2f6ab7c15054c5cdc10de9ceb49c59288c
Nicholas Cantelmi <nickcantelmi@gmail.com>
2018-04-01 18:57:28 -0700

    Remove Rails running into issue with Rails 5.1

--- shopper_challenge/app/controllers/application_controller.rb
+++
@@ -1,7 +0,0 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  def current_applicant
-    @_current_applicant ||= Applicant.find_by email: session[:email]
-  end
-end
